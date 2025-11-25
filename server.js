f74ddbc9a2e8dffb616f0c83bd5f756d4d1a4c84
-A <1992anjagi@gmail.com>
2025-11-25 13:57:40 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import studentRoutes from "./routes/studentRoutes.js";
+import lecturerRoutes from "./routes/lecturerRoutes.js";
+import cors from "cors";
+
+dotenv.config();
+connectDB(); // connect to MongoDB
+
+const app = express();
+app.use(cors());
+app.use(express.json()); // parse JSON bodies
+app.use("/api/students", studentRoutes);
+app.use("/api/lecturers", lecturerRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Congratulations! API is running and connected to MongoDB...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
